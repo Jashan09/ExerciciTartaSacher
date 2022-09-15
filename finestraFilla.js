9b67d226d182a0442582018bcffaa147df9c20fe
daw25 <navjashan.singh@cirvianum.cat>
2022-09-15 19:59:57 +0200

    peces que sobren

--- finestraFilla.js
+++ finestraFilla.js
@@ -5,34 +5,70 @@ function crearRajolaDeXocolata(){
     let coordenadaX = document.getElementById("n").value;
     let coordenadaY = document.getElementById("m").value;
 
+    let totalPecesDemanades = document.getElementById("r").value;
 
     let finestraPare = this.opener;
 
     if(finestraPare){
 
+        console.log(coordenadaX);
+        console.log(coordenadaY);
+
+
+
+        let textProva = "";
+
+        for (let x = 0; x < coordenadaX; x++) {
+            textProva += "🟫 ";
+        }
+
+        console.log(textProva);
+
+        let totalPecesCoordenadaX = textProva;
+
+        textProva += '\n';
+
+        console.log(textProva);
+
+        for (let y = 0; y < coordenadaY-1; y++) {
+            textProva += totalPecesCoordenadaX;
+            textProva += "\n";
+        }
+
+        console.log(textProva);
+
+        let xocolataFinal = textProva;
+
 
         let paragrafFinal = finestraPare.document.getElementById("paragrafDeLaFinestraPare");
-        paragrafFinal.innerText = "";
+        paragrafFinal.innerText = textProva;
+
 
 
         /*
-X X
-X X
-X X
-X X
-         */
+
+n 2
+m 4
+r 10
+
+ */
+
+
+        let pecesEnUnaSolaXocolata = coordenadaX*coordenadaY;
 
 
 
-        let div1 = finestraPare.document.createElement("div")
-        finestraPare.document.body.appendChild(div1);
-        div1.class="text";
+        do{
+            textProva += "\n";
+            textProva += "\n";
+            textProva += xocolataFinal;
+            paragrafFinal.innerText += textProva;
+        }while(totalPecesDemanades>pecesEnUnaSolaXocolata);
 
 
-        finestraPare.document.getElementById("childrenAge").innerHTML
-            =`Diu que es diu <strong>${age}</strong>`;
-        finestraPare.document.getElementById("childrenName").innerHTML
-            =`Diu que es diu <strong>${name}</strong>`;
+
+
+
 
 
     }
